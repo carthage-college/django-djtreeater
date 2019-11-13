55714169e8c2e9313335f46c9b4a8112651828f7
dsullivan <dsullivan@carthage.edu>
2019-11-13 08:16:30 -0600

    base settings

---
+++ djtreeater/settings/base.py
@@ -0,0 +1,238 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+# sqlserver connection string
+from djimix.settings.local import MSSQL_EARL
+from djimix.settings.local import INFORMIX_ODBC, INFORMIX_ODBC_TRAIN
+from djimix.settings.local import (
+    INFORMIXSERVER,
+    DBSERVERNAME,
+    INFORMIXDIR,
+    ODBCINI,
+    ONCONFIG,
+    INFORMIXSQLHOSTS,
+    LD_LIBRARY_PATH,
+    LD_RUN_PATH
+)
+
+# Debug
+DEBUG = False
+INFORMIX_DEBUG = 'debug'
+ADMINS = (
+    ('', ''),
+)
+MANAGERS = ADMINS
+
+SECRET_KEY = ''
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/Chicago'
+USE_I18N = False
+USE_L10N = False
+USE_TZ = False
+DEFAULT_CHARSET = 'utf-8'
+FILE_CHARSET = 'utf-8'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(__file__)
+DATABASES = {
+    'default': {
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'django_djtreeater',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': '',
+        'PASSWORD': ''
+    },
+}
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'djtreeater.core',
+    'djtools',
+]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            '/data2/django_templates/djcher/',
+            '/data2/django_templates/django-djskins/',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug':DEBUG,
+            'context_processors': [
+                'djtools.context_processors.sitevars',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            #'loaders': [
+            #    # insert your TEMPLATE_LOADERS here
+            #]
+        },
+    },
+]
+# caching
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        #'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        #'LOCATION': '127.0.0.1:11211',
+        #'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        #'LOCATION': '/var/tmp/django_djtreeater_cache',
+        #'TIMEOUT': 60*20,
+        #'KEY_PREFIX': 'DJTREEATOR_',
+        #'OPTIONS': {
+        #    'MAX_ENTRIES': 80000,
+        #}
+    }
+}
+# SMTP settings
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_FAIL_SILENTLY = False
+DEFAULT_FROM_EMAIL = ''
+SERVER_EMAIL = ''
+SERVER_MAIL=''
+# Adirondack Application
+ADIRONDACK_TXT_OUTPUT = ''
+ADIRONDACK_JPG_OUTPUT = ''
+ADIRONDACK_ZIP_OUTPUT = ''
+ADIRONDACK_ARCHIVED = ''
+ADIRONDACK_ROOM_ARCHIVED = ''
+ADIRONDACK_TO_EMAIL = ''
+ADIRONDACK_ASCII_EMAIL = ''
+ADIRONDACK_FROM_EMAIL = ''
+ADIRONDACK_LIS_SUPPORT = ''
+ADIRONDACK_HOST = ''
+ADIRONDACK_PORT = 0
+ADIRONDACK_USER =''
+ADIRONDACK_PASS =''
+ADIRONDACK_TEST_API_SECRET = ''
+ADIRONDACK_API_SECRET = ''
+ADIRONDACK_ROOM_FEES = ''
+ADIRONDACK_ROOM_ASSIGNMENTS = ''
+ADIRONDACK_APPLICATONS = ''
+# ADIRONDACK_ACCESS_KEY = ''
+# ADIRONDACK_SECRET = ''
+# ADIRONDACK_BUCKET = ''
+# logging
+LOG_FILEPATH = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'logs/')
+DEBUG_LOG_FILENAME = LOG_FILEPATH + 'debug.log'
+INFO_LOG_FILENAME = LOG_FILEPATH + 'info.log'
+ERROR_LOG_FILENAME = LOG_FILEPATH + 'error.log'
+CUSTOM_LOG_FILENAME = LOG_FILEPATH + 'custom.log'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format' : '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt' : '%Y/%b/%d %H:%M:%S'
+        },
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
+            'datefmt' : '%Y/%b/%d %H:%M:%S'
+        },
+        'custom': {
+            'format': '%(asctime)s: %(levelname)s: %(message)s',
+            'datefmt' : '%m/%d/%Y %I:%M:%S %p'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'custom_logfile': {
+            'level':'ERROR',
+            'filters': ['require_debug_true'], # do not run error logger in production
+            'class': 'logging.FileHandler',
+            'filename': CUSTOM_LOG_FILENAME,
+            'formatter': 'custom',
+        },
+        'info_logfile': {
+            'level':'INFO',
+            'class':'logging.handlers.RotatingFileHandler',
+            'backupCount': 10,
+            'maxBytes': 50000,
+            'filters': ['require_debug_false'], # run logger in production
+            'filename': INFO_LOG_FILENAME,
+            'formatter': 'simple',
+        },
+        'debug_logfile': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'], # do not run debug logger in production
+            'class': 'logging.FileHandler',
+            'filename': DEBUG_LOG_FILENAME,
+            'formatter': 'verbose'
+        },
+        'error_logfile': {
+            'level': 'ERROR',
+            'filters': ['require_debug_true'], # do not run error logger in production
+            'class': 'logging.FileHandler',
+            'filename': ERROR_LOG_FILENAME,
+            'formatter': 'verbose'
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'include_html': True,
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'djtreeater': {
+            'handlers':['error_logfile'],
+            'propagate': True,
+            'level': 'ERROR'
+         },
+        'error_logger': {
+            'handlers': ['error_logfile'],
+            'level': 'ERROR'
+         },
+        'info_logger': {
+            'handlers': ['info_logfile'],
+            'level': 'INFO'
+        },
+        'debug_logger': {
+            'handlers':['debug_logfile'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}

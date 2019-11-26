4159573bcb324d3c8a6c87a5e7d12799e8fb6dd6
dsullivan <dsullivan@carthage.edu>
2019-11-26 11:43:15 -0600

    Added trap for empty file

--- djtreeater/core/adiron_asgn_ntfy.py
+++ djtreeater/core/adiron_asgn_ntfy.py
@@ -15,12 +15,12 @@ django.setup()
 
 from django.conf import settings
 from email.mime.text import MIMEText
-from djtools.utils.mail import send_mail
+# from djtools.utils.mail import send_mail
 from djimix.core.utils import get_connection, xsql
 
 from djtreeater.sql.adirondack import ADIRONDACK_QUERY, Q_GET_TERM
 from djtreeater.core.utilities import fn_get_bill_code, \
-    fn_translate_bldg_for_adirondack, fn_write_error
+    fn_translate_bldg_for_adirondack, fn_write_error, fn_send_mail
 
 # informix environment
 os.environ['INFORMIXSERVER'] = settings.INFORMIXSERVER
@@ -40,38 +40,12 @@ desc = """
     Collect adirondack data Room assignments for stu_serv_rec
 """
 parser = argparse.ArgumentParser(description=desc)
-#
-# def fn_send_mail(to, frum, body, subject):
-#     # email to addresses may come as list
-#     # Stock sendmail in core does not have reply to or split of to emails
-#     msg = MIMEText(body)
-#     msg['To'] = to
-#     msg['From'] = frum
-#     msg['Subject'] = subject
-#     msg['Reply-To'] = frum
-#
-#     print("ready to send")
-#     server = smtplib.SMTP('localhost')
-#     # show communication with the server
-#     # if debug:
-#     #     server.set_debuglevel(True)
-#     try:
-#         # print(msg['To'])
-#         # print(msg['From'])
-#         server.sendmail(frum, to.split(','), msg.as_string())
-#
-#     finally:
-#         server.quit()
-#         # print("Done")
-#         pass
-
 
 def fn_get_name(id, EARL):
     fname = ""
     Q_GET_NAME = '''select fullname from id_rec 
        where id = {0}'''.format(id)
 
-    # ret = do_sql(Q_GET_NAME, key=DEBUG, earl=EARL)
     connection = get_connection(EARL)
     # connection closes when exiting the 'with' block
     with connection:
@@ -82,43 +56,39 @@ def fn_get_name(id, EARL):
     ret = list(data_result)
 
     if ret is None:
-        # row = ret.fetchone()
-        # if row is None:
-            print("Name not found")
-            fn_write_error(
-                "Error in asign_notify.py - fn_get namen: No "
-                "name found ")
-            quit()
-            fname = str(id)
-        else:
-            for row in ret:
-                fname = row[0]
+        # print("Name not found")
+        fn_write_error(
+            "Error in asign_notify.py - fn_get namen: No "
+            "name found ")
+        quit()
+        fname = str(id)
+    else:
+        for row in ret:
+            fname = row[0]
 
     return fname
 
 def fn_notify(file, EARL):
     try:
         from operator import itemgetter
-        # This needs to be pointed to the d2 data folder
         room_file = file
         # room_file = 'assignment.csv'
-        print(room_file)
         r = csv.reader(open(room_file))
+        """
+        I am creating a room assignment csv on each day of changes and
+        sending the bill code to CX
+        Make comparisons based on that code in the csv file 
+        after the assignment.csv is created, read the csv back into
+        memory sorted by student ID and date
+        Changes will have a posted code of 2 and a second record coded 0
+        New entries will only have one record coded 0
+        Loop through that set and determine if a room has changed and the
+        bill code has also changed.
+        """
+        code_list = []
+        xtra_list = []
 
-        # I am creating a room assignment csv on each day of changes and
-        # sending the bill code to CX
-        # Can easily add bill code to the csv file and make comparisons based
-        # on that code, the csv file is only used for history and auditing...
-        # so after the assignment.csv is created, read the csv back into
-        # memory sorted by student ID and date
-        # Changes will have a
-        # Loop through that set and determine if a room has changed and the
-        # bill code has also changed.
-
-        code_list = [""]
-        xtra_list = [""]
-
-        lastid = ""
+        lastid = 0
         lastpost = 0
         lastdate = ""
         lastroom = ""
@@ -126,95 +96,99 @@ def fn_notify(file, EARL):
         lastbldg = ""
         lastcode = ""
         fullname = ""
+
         for line in sorted(r, key=itemgetter(0, 10)):
 
-            print(line[0] + " " + line[10] + " " +  str(line[21]))
-
-            # Skip the first line
-            if line[0] == "STUDENTNUMBER":
+            """Skip the header line - because of sort, it may not be 
+            the first line in the record"""
+            # if not unicode(line[0]).isnumeric() and lastid == 0:
+            if not line[0].isnumeric():
+                # print("Skip header")
+                # if line[0] == "STUDENTNUMBER":
                 pass
-            elif line[0] != lastid:
+
+            else:
+                post = line[21]
+                # print("last id = " + str(lastid))
+                """Revision 11/21/19 - records can come as a change, which
+                  pulls two records from the API, or an add, which pulls
+                  only one.   An add needs to be accounted for as a code
+                  change, so I've reworked the logic"""
+
+                if post == "2":
+                    # print("First record of change")
+                    """Same student, different row, change
+                        write the record to the notification
+                        set the passcount to 2
+                    """
+                elif post == "0":
+                    if line[0] == lastid:
+                        """this is the second record of a change"""
+                        if lastcode == line[23]:
+                            code_list.append("Student " + line[0]
+                                    + ", " + fullname + " moved to "
+                                    + line[2] + " " + line[4] + " " + line[23]
+                                    + ", " + line[6] + " from " + lastbldg
+                                    + " " + lastroom + " "
+                                    + lastcode + ", " + lastroomtype
+                                    + " on " + line[10])
+                        else:
+                            xtra_list.append("Student " + line[0]
+                                    + ", " + fullname + " moved to " + line[2]
+                                    + " " + line[4] + " " + line[23] + ", "
+                                    + line[6] + " from " + lastbldg + " "
+                                    + lastroom + " " + lastcode + ", "
+                                    + lastroomtype + " on " + line[10])
+
+                elif line[0] != lastid:
+                    """The first assignment   """
+                    fullname = fn_get_name(lastid, EARL)
+                    code_list.append("Student " + lastid
+                                     + ", " + fullname
+                                     + " initially assigned to "
+                                     + lastbldg + " " + lastroom + " "
+                                     + lastcode + ", " + lastroomtype
+                                     + " on " + lastdate)
+
+                # store this row and go to the next record
                 lastid = line[0]
-                lastpost = line[21]
                 lastdate = line[10]
                 lastroomtype = line[6]
                 lastcode = line[23]
                 lastbldg = line[2]
                 lastroom = line[4]
-                print("first record " + lastid + " " + lastpost + " "
-                  + lastdate + " " + lastroom + " " + lastcode)
-            else:
-                if line[6] != lastroomtype:
-                    print("Change to " + line[23])
-                    # go get student name here...
-                    fullname = ""
-                    Q_GET_NAME = '''select fullname from id_rec 
-                       where id = {0}'''.format(line[0])
 
-                    connection = get_connection(EARL)
-                    # connection closes when exiting the 'with' block
-                    with connection:
-                        data_result = xsql(
-                            Q_GET_NAME, connection,
-                            key=settings.INFORMIX_DEBUG
-                        ).fetchall()
-                    ret = list(data_result)
-                    # ret = do_sql(Q_GET_NAME, key=DEBUG, earl=EARL)
-
-                    if ret is None:
-                        print("Name not found")
-                        # fn_write_error(
-                        #     "Error in asign_notify.py - fn_notifyn: No "
-                        #     "name found ")
-                        quit()
-                    else:
-                        for row in ret:
-                            fullname = row[0]
-
-                    code_list.append("Student " + line[0]
-                            + ", " + fullname + " moved to " + line[2] + " "
-                            + line[4] + " " + line[23] + ", " + line[6]
-                            + " from " + lastbldg + " " + lastroom + " "
-                            + lastcode + ", " + lastroomtype
-                            + " on " + line[10])
-                else:
-                    print("Not a change")
-                    xtra_list.append("Student " + line[0]
-                                     + ", " + fullname
-                                     + " moved to " + line[2] + " " + line[4]
-                                     + " " + line[23]
-                                     + ", " + line[6]
-                                     + " from "
-                                     + lastbldg + " " + lastroom + " "
-                                     + lastcode + ", " + lastroomtype
-                                     + " on " + line[10])
-
-        body = "\n" + "Room changes requiring bill code change:"
-        for i in code_list:
-            body = body + i + "\n"
-
-        if xtra_list is not None:
-            body = body + "\n" + "Room changes not affecting bill code:"
-            for i in xtra_list:
+        """PREPARE THE EMAIL"""
+        if len(code_list) == 0:
+            # print("No changes in file")
+            quit()
+        else:
+            body = "\n" + "Room changes requiring bill code change:" + "\n"
+            for i in code_list:
                 body = body + i + "\n"
 
-        frum = settings.ADIRONDACK_FROM_EMAIL
-        # # tu = settings.ADIRONDACK_TO_EMAIL
-        tu = 'dullivan@carthage.edu'
-        # # tu = settings.ADIRONDACK_ASCII_EMAIL  #has Marietta and Carol
-        subj = "Adirondack - Room Bill Code Change"
+            if xtra_list is not None:
+                body = body + "\n" + "Room changes not affecting bill code:" \
+                       + "\n"
+                for i in xtra_list:
+                    body = body + i + "\n"
 
-        # mail not working as of 11/15/19 - debug
-        # # fn_send_mail(tu, frum, body, subj)
+            frum = settings.ADIRONDACK_FROM_EMAIL
+            tu = settings.ADIRONDACK_ASCII_EMAIL
+            # tu = 'dsullivan@carthage.edu'
+            subj = "Adirondack - Room Bill Code Change"
 
-        print("Mail Sent " + subj + " TO:" + str(tu) + " FROM:" + str(frum)
-              + " DETAILS: " + "\n" + body)
+            fn_send_mail(tu, frum, body, subj)
+
+            # print("Mail Sent " + subj + " TO:" + str(tu) + " FROM:" + str(frum)
+            #       + " DETAILS: " + "\n" + body)
+
 
     except Exception as e:
         print(
-                "Error in assign_notify.py:  " +                 e.message)
+                "Error in assign_notify.py:  " + repr(e))
         # fn_write_error(
-        #     "Error in assign_notify.py:" + e.message)
+        #     "Error in assign_notify.py:" + repr(e))
 
 
 # def main():

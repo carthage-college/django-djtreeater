33ce66d3506ab8bf4cf961eb10e2fc2a133a8c2e
dsullivan <dsullivan@carthage.edu>
2020-01-16 13:51:03 -0600

    corrected db reference

--- djtreeater/bin/room_assignments.py
+++ djtreeater/bin/room_assignments.py
@@ -106,7 +106,7 @@ def main():
         global EARL
         # determines which database is being called from the command line
         if database == 'cars':
-            EARL = settings.INFORMIX_ODBC_TRAIN
+            EARL = settings.INFORMIX_ODBC
         if database == 'train':
             EARL = settings.INFORMIX_ODBC_TRAIN
         elif database == 'sandbox':

6c2acd4900aa37b1864dd5a8197401b27334c5ae
dsullivan <dsullivan@carthage.edu>
2020-02-05 16:23:46 -0600

    Working on logic and sql tweaking

--- djtreeater/bin/manage_stu_serv_rec.py
+++ djtreeater/bin/manage_stu_serv_rec.py
@@ -10,8 +10,8 @@ django.setup()
 
 from django.conf import settings
 from djimix.core.utils import get_connection, xsql
-from djtreeater.sql.stu_serv_rec import get_spring_to_fall, get_fall_to_spring, \
-    insert_ssr, last_ssr
+from djtreeater.sql.stu_serv_rec import get_spring_to_fall, \
+    get_fall_to_spring, insert_ssr, get_last_ssr
 from djtreeater.core.stu_serv_utils import fn_set_term_vars
 
 import argparse
@@ -94,10 +94,10 @@ def main():
     """This will look for records in the stu_acad_rec that do not exist in
     the stu_serv_rec and will create a basic entry
     
-    Incoming first time students will get a basic entry w/o bldg, room, parking,
-    bill code, meal plan  
-    Should start looking Nov 1 for upcoming spring term and March 15 for upcoming
-    fall term
+    Incoming first time students will get a basic entry w/o bldg, room, 
+    parking,bill code, meal plan  
+    Should start looking Nov 1 for upcoming spring term and March 15 
+    for upcoming fall term
     
     Returning students will get a basic minimal entry for the fall term
     
@@ -136,82 +136,61 @@ def main():
         ).fetchall()
     cur_ssr = list(data_result)
     # print(cur_ssr)
+
     if len(cur_ssr) != 0:
         for row in cur_ssr:
             print('----------------')
-            print("Stu Serv Rec Found for " + str(row[0]))
-            print(row)
+            print("Stu Serv Rec needed for " + str(row[0]))
+            # print(row)
             carth_id = row[0]
-            stu_cl = row[4]
+            # stu_cl = row[9]
+            # earn_hrs = row[6]
 
-    """
-            IF cl = 'FF' and 'cum_earn_hrs = 0
-                Clean insert
-                (By definition, such would not have a prior stu serv rec
-                  so the cl is redundant logic.  If they have a stu acad rec,
-                  they will need a stu serv rec and there would be nothing
-                  to pull from the prior term)
-            IF had a room last term...
-                (For fall to spring, always copy the last term.  For spring to
-                fall ignore)
-            IF nothing from previous term 
-                clean insert
-            If fall term upcoming
-                clean insert
-            If spring term coming and fall term populated
-                Copy fall to spring
-                    """
-
-    if target_sess == 'RA' or stu_cl == 'FF':
-        print("clean insert - no need to use last term")
-        x = insert_ssr(carth_id, target_sess, target_yr, "UN", "000",
-                       "", "R", "R")
-        print(x)
-    else:
-        print("search previous term stu_serv_rec")
-        """This query will find the prior stu_serv_rec if it exists"""
-        last_ssr_sql = last_ssr
-
-        #     # IF WE WANTED TO INCLUDE INCOMING....
-        #     # select
-        #     # ADM.id, ADM.plan_enr_sess, ADM.plan_enr_yr, ADM.primary_app,
-        #     # ADM.enrstat
-        #     # from adm_rec ADM
-        #     # where
-        #     # ADM.plan_enr_sess = "RC"
-        #     # and ADM.plan_enr_yr = 2020
-        #     # and ADM.enrstat in ('DEPOSIT', 'ADMITTED')
-        #     # and ADM.primary_app = 'Y'
-        #
-        print(last_ssr_sql)
-        connection = get_connection(EARL)
-        """ connection closes when exiting the 'with' block """
-        with connection:
-            data_result = xsql(
-                last_ssr_sql, connection,
-                key=settings.INFORMIX_DEBUG
-            ).fetchall()
-            last_ssr = list(data_result)
-            if len(last_ssr) != 0:
-                print("Stu Serv Rec Found")
-                print ("Can use previous term")
-                for r in last_ssr:
-                    billcode = r[9]
-                    bldg = r[5]
-                    room = r[6]
-                    intdhsg = r[3]
-                    rsvstat = r[10]
-                    # mealplan = r[7]
-                    # parkloc = r[8]
-
-                # x = insert_ssr(carth_id, target_sess, target_yr, bldg,
-                #                room, billcode, intdhsg, rsvstat)
-                # print(x)
+            """Fall term is always a clean insert"""
+            if target_sess == 'RA':
+                print("clean insert - no need to use last term")
+                x = insert_ssr(carth_id, target_sess, target_yr, "UN", "000",
+                               "", "R", "R")
+                print(x)
             else:
-                print("No prior rec - insert clean")
-                # x = insert_ssr(carth_id, target_sess, target_yr,
-                #                "UN", "UN", "", "R", "R")
-                # print(x)
+                print("search previous term stu_serv_rec")
+                """This query will find the prior stu_serv_rec if it exists"""
+                last_ssr_sql = get_last_ssr(carth_id, last_yr, last_sess)
+
+                # print(last_ssr_sql)
+                connection = get_connection(EARL)
+                """ connection closes when exiting the 'with' block """
+                with connection:
+                    data_result = xsql(
+                        last_ssr_sql, connection,
+                        key=settings.INFORMIX_DEBUG
+                    ).fetchall()
+                    last_ssr = list(data_result)
+                    if len(last_ssr) != 0:
+                        print("Stu Serv Rec Found")
+                        print ("Can use previous term")
+                        for r in last_ssr:
+                            billcode = r[9]
+                            bldg = r[5]
+                            room = r[6]
+                            intdhsg = r[3]
+                            rsvstat = r[10]
+                            # mealplan = r[7]
+                            # parkloc = r[8]
+                        # print ("Insert " + str(carth_id), target_sess,
+                        #        str(target_yr), bldg, room, billcode,
+                        #        intdhsg, rsvstat)
+                        # x = insert_ssr(carth_id, target_sess, target_yr, bldg,
+                        #                room, billcode, intdhsg, rsvstat)
+                        # print(x)
+                    else:
+                        print("No prior rec - insert clean")
+                        # x = insert_ssr(carth_id, target_sess, target_yr,
+                        #                "UN", "UN", "", "R", "R")
+                        # print(x)
+
+    else:
+        print("Nothing to do")
 
 
 

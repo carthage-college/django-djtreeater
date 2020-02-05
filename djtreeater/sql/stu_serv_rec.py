45599778ea7b9e97c773aa0555a2f16497ce9334
dsullivan <dsullivan@carthage.edu>
2020-02-05 16:25:47 -0600

    Reworked the  SQL

--- djtreeater/sql/stu_serv_rec.py
+++ djtreeater/sql/stu_serv_rec.py
@@ -1,143 +1,111 @@
 
 def get_fall_to_spring(target_sess, target_yr):
-    SQL_FALL_TO_SPRING = '''
-    SELECT id, prog, subprog, vw_acst, pr_acst, stu_group, 
-            reg_hrs, earn_hrs, pr_cl, ar_cl 
-            FROM
-            (
-            -- Get upperclassmen
-            SELECT CSV.id, CSV.prog, CSV.subprog, CSV.acst vw_acst, 
-            CSV.stu_group, SAR.reg_hrs, SAR.cl ar_cl, SAR.earn_hrs, 
-            PER.acst pr_acst, PER.cl pr_cl 
-            FROM
-            cc_current_students_vw CSV 
-            JOIN prog_enr_rec PER 
-            ON PER.id = CSV.id AND PER.subprog = 'TRAD' 
-            JOIN stu_acad_rec SAR 
-            ON CSV.id = SAR.id AND PER.prog = SAR.prog 
-                AND PER.subprog = SAR.subprog 
-                AND SAR.sess = '{0}'
-                AND SAR.yr = {1}
-                AND SAR.subprog = 'TRAD' 
-                AND SAR.cl != 'SP' 
-                --Exclude graduating 
-                AND CSV.id NOT IN
-                (
-                   SELECT id FROM prog_enr_rec PER 
-                WHERE 
-                (PER.plan_grad_grp in ('DCST', 'JAN')
-                AND PER.plan_grad_sess in ('RB') --arg_session_fall    
-                AND PER.plan_grad_yr = {1}) --arg_year_fall
-                OR
-                (PER.plan_grad_grp in ('DEC')
-                AND PER.plan_grad_sess in ('RA') --arg_session_fall    
-                AND PER.plan_grad_yr = {1}-1) --arg_year_fall
-                )
-            UNION --INCOMING
-            SELECT CSV.id, CSV.prog, CSV.subprog, CSV.acst vw_acst, 
-            CSV.stugroup, SAR.reg_hrs, SAR.cl ar_cl, SAR.earn_hrs, 
-            PER.acst pr_acst, PER.cl pr_cl 
-            FROM cc_incoming_students_vw CSV 
-            JOIN prog_enr_rec PER 
-            ON PER.id = CSV.id 
-                AND    PER.subprog = 'TRAD' 
-            JOIN stu_acad_rec SAR 
-            ON CSV.id = SAR.id 
-                AND PER.prog = SAR.prog 
-                AND PER.subprog = SAR.subprog 
+    """FIND ALL CURRENT STUDENTS WHO ARE ENROLLED OR REGISTERED FOR THE
+        UPCOMING SPRING TERM OR WERE REGISTERED IN THE FALL ???
+        AND EXCLUDE GRADUATING STUDENTS
+        AND STUDENTS WHO ALREADY HAVE THE STU_SERV_REC FOR THE TERM"""
+    SQL_FALL_TO_SPRING = '''SELECT
+            id, prog, subprog, acst, 
+            reg_hrs, ar_cl, earn_hrs, sess, yr, cl
+         FROM
+             (
+             SELECT
+            PER.id, PER.prog, PER.subprog, PER.acst, 
+            SAR.reg_hrs, SAR.cl ar_cl, SAR.earn_hrs, SAR.sess, SAR.yr, PER.cl
+                FROM
+                    prog_enr_rec    PER    
+                LEFT JOIN        stu_acad_rec    SAR    
+                    ON    PER.id        =    SAR.id
+                    AND    PER.subprog    =    SAR.subprog
+                    AND    SAR.yr        =  {1}  --    arg_year_spring
+                    AND    SAR.sess        =    '{0}' --arg_session_spring
+     
+                 WHERE
+                    PER.subprog    =    'TRAD'
                 AND
-                (
-                    (SAR.sess = '{0}'
-                        AND SAR.yr = {1} 
-                        AND SAR.reg_hrs > 0 
-                        AND SAR.subprog = 'TRAD'
+                    --Student should be registered for 1 or more credit hours
+                    -- in the spring
+                    NVL(SAR.reg_hrs, 0)    >=    1
+                AND
+                    --Not graduating in December or January
+                    (
+                        NOT (PER.plan_grad_grp  IN  ('DCST','JAN')    
+                        AND    PER.plan_grad_sess   =    'RB'    
+                        AND    PER.plan_grad_yr  = {1} )  --arg_year_spring)
+                        OR
+                        NOT(PER.plan_grad_grp  =  'DEC'    
+                        AND    PER.plan_grad_sess  =  'RA' --arg_session_fall    
+                        AND    PER.plan_grad_yr  =  {1} - 1 ) --arg_year_fall)
                     )
-                    OR --Check for previous IN case they took a semester off
-                    (SAR.sess = "RC" 
-                    AND SAR.yr = {1} 
-                    AND SAR.reg_hrs > 0 
-                    AND SAR.subprog = 'TRAD'
-                    )    
-                )
-    )
-    --Exclude if they already have a stu_serv_rec for the term
-    WHERE id NOT IN
+                UNION
+                SELECT
+                    PER.id, 
+                    PER.prog, PER.subprog, PER.acst, 
+                     0 reg_hrs, PER.cl, 0 earn_hrs, PER.adm_sess sess, 
+                     PER.adm_yr yr, PER.cl
+                FROM
+                    prog_enr_rec    PER
+                WHERE
+                    PER.adm_yr        =    {1}   --arg_year_spring
+                AND
+                    PER.adm_sess    IN    ('RB','RC')
+                AND
+                    PER.subprog        =    'TRAD'
+                AND
+                    PER.lv_date        IS    NULL
+                AND
+                    PER.cl            <>    'SP'
+            )
+        WHERE id NOT IN
     (SELECT id 
     FROM stu_serv_rec 
-    WHERE sess = '{0}' AND yr = 2019)
+    WHERE sess = '{0}' AND yr = {1} ) 
+
             '''.format(target_sess, target_yr)
 
     return SQL_FALL_TO_SPRING
 
 def get_spring_to_fall(target_sess, target_yr):
+    """FIND ALL CURRENT STUDENTS WHO ARE ENROLLED OR REGISTERED FOR THE
+         UPCOMING FALL TERM OR WERE REGISTERED IN THE SPRING ???
+         AND EXCLUDE GRADUATING STUDENTS
+         AND STUDENTS WHO ALREADY HAVE THE STU_SERV_REC FOR THE TERM"""
 
-    SQL_SPRING_TO_FALL = '''SELECT
-     id, prog, subprog, vw_acst, pr_acst, stu_group, reg_hrs, earn_hrs, 
-    pr_cl,  ar_cl
-    FROM 
-    (
-        -- Get upperclassmen
-        SELECT CSV.id, CSV.prog, CSV.subprog, CSV.acst vw_acst, 
-                CSV.stu_group, SAR.reg_hrs, 
-           SAR.cl ar_cl, SAR.earn_hrs,
-           PER.acst pr_acst, PER.cl pr_cl
-        FROM cc_current_students_vw CSV   --Current students only
-        JOIN prog_enr_rec PER             --Enrolled in a program
-        ON PER.id = CSV.id
-        AND PER.subprog = 'TRAD'
-
-        JOIN stu_acad_rec SAR             --Registered
-        ON CSV.id = SAR.id
-        AND PER.prog = SAR.prog
-        AND PER.subprog = SAR.subprog
-        AND SAR.sess = '{0}' and SAR.yr = {1}    --Session to be queried
-        AND SAR.subprog = 'TRAD'                 --TRAD only
-        AND SAR.cl != 'SP'                       --No JTerm trip only students
-
-        --DO NOT INCLUDE those graduating in the spring (source term)
-        AND CSV.id NOT IN
+    SQL_SPRING_TO_FALL = '''
+    SELECT PER.id, SAR.prog, SAR.subprog, SAR.acst, 
+            SAR.reg_hrs, SAR.cl ar_cl, SAR.earn_hrs, SAR.sess, SAR.yr, PER.cl
+              --INTO v_cx_id
+        FROM
+            prog_enr_rec    PER    
+        LEFT JOIN    stu_acad_rec    SAR    
+            ON    PER.id        =    SAR.id
+            AND    SAR.yr        =    {1}  --arg_year
+            AND    SAR.sess    =    '{0}'
+        WHERE
+            --Get students with an active academic standing
+            PER.acst    IN    ('ACPR','CIC','GOOD','LOC','PROB','PROC','PROR',
+                'READ','SAB','SHAC','SHOC')
+        AND
+            --Who are traditional students
+            PER.subprog    =    'TRAD'
+        AND
             (
-            SELECT id FROM prog_enr_rec PER
-            WHERE 
-                    (PER.plan_grad_grp IN ('MAY','SUM', 'MYST') 
-                    AND PER.plan_grad_sess in ('RC', 'RE')
-                    AND PER.plan_grad_yr = {1}) 
+                --Are either incoming freshmen/transfers
+                (NVL(PER.adm_yr,0)    =  {1}   --    arg_year    
+                AND    NVL(PER.adm_sess,'')    =    '{0}')
+                OR
+                --Have registered for 1 or more credits in the coming session
+                NVL(SAR.reg_hrs, 0)            >=    1
             )
-        
-        UNION
-        --INCOMING
-        SELECT
-        CSV.id, CSV.prog, CSV.subprog, CSV.acst vw_acst, 
-                CSV.stugroup, SAR.reg_hrs, 
-           SAR.cl ar_cl, SAR.earn_hrs,
-           PER.acst pr_acst, PER.cl pr_cl
-        FROM cc_incoming_students_vw CSV
-        JOIN prog_enr_rec PER
-        ON PER.id = CSV.id
-        AND PER.subprog = 'TRAD'
-
-        JOIN stu_acad_rec SAR
-        ON CSV.id = SAR.id
-        AND PER.prog = SAR.prog
-        AND 
-            (
-                (SAR.sess = '{0}' and SAR.yr = {1} 
-                        AND SAR.reg_hrs > 0 
-                        AND SAR.subprog = 'TRAD')
-                OR  --Check for previous in case they took a semester off
-                (SAR.sess = "RC" 
-                        and SAR.yr = {1} 
-                        AND SAR.reg_hrs > 0 
-                        AND SAR.subprog = 'TRAD')
-            )
-)
-    --Exclude those already in stu_serv_Rec
-        WHERE id NOT IN 
-        (SELECT id FROM stu_serv_rec
-        WHERE sess = '{0}' and yr = {1}
-        )
-
-               
+        AND
+            --And will not be graduating before spring
+            NVL(PER.plan_grad_sess,'') || NVL(PER.plan_grad_yr,'') || NVL(PER.plan_grad_grp,'')    
+            NOT IN    ('RC' || {1} || 'MAY', 'RE' || {1} || 'SUM', 'RC' || {1} || 'MYST')
+            
+        AND PER.id NOT IN
+    (SELECT id 
+    FROM stu_serv_rec 
+    WHERE sess = '{0}' AND yr = {1}) 
         '''.format(target_sess, target_yr)
 
     return SQL_SPRING_TO_FALL
@@ -172,7 +140,7 @@ def insert_ssr(id, sess, yr, bldg, room, billcode, intendhsg, rsvstat):
         print("Error on insert " + repr(e))
         return 0
 
-def last_ssr(carth_id, last_yr, last_sess):
+def get_last_ssr(carth_id, last_yr, last_sess):
     last_ssr_sql = '''select id, sess, yr, intend_hsg, campus,
                     bldg, room, meal_plan_type, park_location, bill_code,
                     rsv_stat
